1adec9b4287ee41d8f30e4225831d9ea35bb2cfa
Guru Duan <ridhwan018@gmail.com>
2026-07-10 10:46:03 +0800

    perbaikan waktu untuk hari jumat

--- jam_pelajaran.php
+++ jam_pelajaran.php
@@ -18,55 +18,131 @@ $PAGE->set_title('Jam Pelajaran');
 $PAGE->set_heading('Jam Pelajaran (JP)');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_sesskey();
+$data = [
 
-    $data = [
+    'mode_aktif' => $_POST['mode_aktif'],
 
-        'jumlah_jam' => $_POST['jumlah_jam'],
+    // ==========================
+    // MODE NORMAL
+    // ==========================
+    'normal' => [
 
-        'jam_mulai' => $_POST['jam_mulai'],
+        // Senin - Kamis
+        'senin_kamis' => [
 
-        'mode_aktif' => $_POST['mode_aktif'],
+            'jumlah_jam' => $_POST['normal_sk_jumlah_jam'],
+            'jam_mulai'  => $_POST['normal_sk_jam_mulai'],
 
-        'mode' => [
+            'durasi_jam' => $_POST['normal_sk_durasi_jam'],
 
-            'normal' => [
+            'ist1_setelah' => $_POST['normal_sk_ist1_setelah'],
+            'ist1_durasi'  => $_POST['normal_sk_ist1_durasi'],
 
-                'durasi_jam' => $_POST['durasi_normal'],
+            'ist2_setelah' => $_POST['normal_sk_ist2_setelah'],
+            'ist2_durasi'  => $_POST['normal_sk_ist2_durasi']
 
-                'ist1_setelah' => $_POST['normal_ist1_setelah'],
-                'ist1_durasi' => $_POST['normal_ist1_durasi'],
+        ],
 
-                'ist2_setelah' => $_POST['normal_ist2_setelah'],
-                'ist2_durasi' => $_POST['normal_ist2_durasi']
+        // Jumat
+        'jumat' => [
 
-            ],
+            'jumlah_jam' => $_POST['normal_jumat_jumlah_jam'],
+            'jam_mulai'  => $_POST['normal_jumat_jam_mulai'],
 
-            'rapat' => [
+            'durasi_jam' => $_POST['normal_jumat_durasi_jam'],
 
-                'durasi_jam' => $_POST['durasi_rapat'],
-
-                'ist1_setelah' => $_POST['rapat_ist1_setelah'],
-                'ist1_durasi' => $_POST['rapat_ist1_durasi'],
-
-                'ist2_setelah' => $_POST['rapat_ist2_setelah'],
-                'ist2_durasi' => $_POST['rapat_ist2_durasi']
-
-            ]
+            'ist1_setelah' => $_POST['normal_jumat_ist1_setelah'],
+            'ist1_durasi'  => $_POST['normal_jumat_ist1_durasi']
 
         ]
 
-    ];
+    ],
 
+    // ==========================
+    // MODE RAPAT
+    // ==========================
+    'rapat' => [
+
+        // hanya Senin-Kamis
+        'senin_kamis' => [
+
+            'jumlah_jam' => $_POST['rapat_jumlah_jam'],
+            'jam_mulai'  => $_POST['rapat_jam_mulai'],
+
+            'durasi_jam' => $_POST['rapat_durasi_jam'],
+
+            'ist1_setelah' => $_POST['rapat_ist1_setelah'],
+            'ist1_durasi'  => $_POST['rapat_ist1_durasi'],
+
+            'ist2_setelah' => $_POST['rapat_ist2_setelah'],
+            'ist2_durasi'  => $_POST['rapat_ist2_durasi']
+
+        ]
+
+    ]
+
+];
     jurnalmengajar_save_config_jam($data);
+    redirect($PAGE->url, 'Pengaturan berhasil disimpan.', 1);
 }
 
 $config = jurnalmengajar_get_config_jam();
 
-$jam = jurnalmengajar_generate_jam();
+/*
+=====================================================
+FUNGSI PREVIEW
+=====================================================
+*/
+
+function tampilkan_preview_jam($judul, $jam) {
+
+    if (empty($jam)) {
+        return;
+    }
+
+    echo "<h3>$judul</h3>";
+
+    echo "<table class='generaltable'>";
+
+    echo "<tr>";
+    echo "<th>Jam</th>";
+    echo "<th>Mulai</th>";
+    echo "<th>Selesai</th>";
+    echo "</tr>";
+
+    foreach ($jam as $j => $w) {
+
+        echo "<tr>";
+
+        echo "<td>$j</td>";
+        echo "<td>{$w['mulai']}</td>";
+        echo "<td>{$w['selesai']}</td>";
+
+        echo "</tr>";
+
+        if (!empty($w['istirahat_setelah'])) {
+
+            echo "<tr style='background:#ffeeba;font-weight:bold;'>";
+
+            echo "<td colspan='3'>";
+
+            echo "Istirahat {$w['istirahat_setelah']} menit";
+
+            echo "</td>";
+
+            echo "</tr>";
+        }
+    }
+
+    echo "</table>";
+}
+
 
 echo $OUTPUT->header();
 
 echo "<form method='post'>";
+echo "<input type='hidden' name='sesskey' value='".sesskey()."'>";
 
 echo "<table class='generaltable'>";
 
@@ -75,20 +151,6 @@ echo "<table class='generaltable'>";
 UMUM
 =====================================================
 */
-
-echo "<tr>";
-echo "<td>Jumlah JP</td>";
-echo "<td>";
-echo "<input type='number' name='jumlah_jam' value='".($config['jumlah_jam'] ?? '')."'>";
-echo "</td>";
-echo "</tr>";
-
-echo "<tr>";
-echo "<td>JP pertama mulai pukul</td>";
-echo "<td>";
-echo "<input type='time' name='jam_mulai' value='".($config['jam_mulai'] ?? '')."'>";
-echo "</td>";
-echo "</tr>";
 
 echo "<tr>";
 echo "<td>Mode Aktif</td>";
@@ -114,93 +176,170 @@ echo "</select>";
 echo "</td>";
 echo "</tr>";
 
+echo "<div class='alert alert-info'>";
+echo "Mode Rapat hanya digunakan untuk hari Senin–Kamis. Hari Jumat tetap menggunakan konfigurasi Normal Jumat.";
+echo "</div>";
+
 /*
 =====================================================
-MODE NORMAL
+NORMAL SENIN-KAMIS
 =====================================================
 */
 
 echo "<tr style='background:#d4edda; font-weight:bold;'>";
-echo "<td colspan='2'>MODE NORMAL</td>";
+echo "<td colspan='2'>NORMAL SENIN-KAMIS</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi JP Mode Normal</td>";
+echo "<td>Jumlah JP</td>";
 echo "<td>";
-echo "<input type='number' name='durasi_normal' value='".($config['mode']['normal']['durasi_jam'] ?? '45')."'>";
+echo "<input type='number' name='normal_sk_jumlah_jam' value='".($config['normal']['senin_kamis']['jumlah_jam'] ?? '11')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Istirahat pertama setelah JP ke (Mode Normal)</td>";
+echo "<td>JP pertama mulai pukul</td>";
 echo "<td>";
-echo "<input type='number' name='normal_ist1_setelah' value='".($config['mode']['normal']['ist1_setelah'] ?? '4')."'>";
+echo "<input type='time' name='normal_sk_jam_mulai' value='".($config['normal']['senin_kamis']['jam_mulai'] ?? '07:30')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi istirahat pertama Mode Normal</td>";
+echo "<td>Durasi JP</td>";
 echo "<td>";
-echo "<input type='number' name='normal_ist1_durasi' value='".($config['mode']['normal']['ist1_durasi'] ?? '15')."'>";
+echo "<input type='number' name='normal_sk_durasi_jam' value='".($config['normal']['senin_kamis']['durasi_jam'] ?? '45')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Istirahat kedua setelah JP ke (Mode Normal)</td>";
+echo "<td>Istirahat pertama setelah JP ke</td>";
 echo "<td>";
-echo "<input type='number' name='normal_ist2_setelah' value='".($config['mode']['normal']['ist2_setelah'] ?? '7')."'>";
+echo "<input type='number' name='normal_sk_ist1_setelah' value='".($config['normal']['senin_kamis']['ist1_setelah'] ?? '4')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi istirahat kedua Mode Normal</td>";
+echo "<td>Durasi istirahat pertama</td>";
 echo "<td>";
-echo "<input type='number' name='normal_ist2_durasi' value='".($config['mode']['normal']['ist2_durasi'] ?? '30')."'>";
+echo "<input type='number' name='normal_sk_ist1_durasi' value='".($config['normal']['senin_kamis']['ist1_durasi'] ?? '20')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Istirahat kedua setelah JP ke</td>";
+echo "<td>";
+echo "<input type='number' name='normal_sk_ist2_setelah' value='".($config['normal']['senin_kamis']['ist2_setelah'] ?? '7')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Durasi istirahat kedua</td>";
+echo "<td>";
+echo "<input type='number' name='normal_sk_ist2_durasi' value='".($config['normal']['senin_kamis']['ist2_durasi'] ?? '30')."'>";
 echo "</td>";
 echo "</tr>";
 
 /*
 =====================================================
-MODE RAPAT
+NORMAL JUMAT
+=====================================================
+*/
+
+echo "<tr style='background:#d1ecf1; font-weight:bold;'>";
+echo "<td colspan='2'>NORMAL JUMAT</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Jumlah JP</td>";
+echo "<td>";
+echo "<input type='number' name='normal_jumat_jumlah_jam' value='".($config['normal']['jumat']['jumlah_jam'] ?? '6')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>JP pertama mulai pukul</td>";
+echo "<td>";
+echo "<input type='time' name='normal_jumat_jam_mulai' value='".($config['normal']['jumat']['jam_mulai'] ?? '07:30')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Durasi JP</td>";
+echo "<td>";
+echo "<input type='number' name='normal_jumat_durasi_jam' value='".($config['normal']['jumat']['durasi_jam'] ?? '35')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Istirahat setelah JP ke</td>";
+echo "<td>";
+echo "<input type='number' name='normal_jumat_ist1_setelah' value='".($config['normal']['jumat']['ist1_setelah'] ?? '4')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Durasi istirahat</td>";
+echo "<td>";
+echo "<input type='number' name='normal_jumat_ist1_durasi' value='".($config['normal']['jumat']['ist1_durasi'] ?? '30')."'>";
+echo "</td>";
+echo "</tr>";
+
+/*
+=====================================================
+RAPAT SENIN-KAMIS
 =====================================================
 */
 
 echo "<tr style='background:#fff3cd; font-weight:bold;'>";
-echo "<td colspan='2'>MODE RAPAT GURU</td>";
+echo "<td colspan='2'>RAPAT SENIN-KAMIS</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi JP Mode Rapat</td>";
+echo "<td>Jumlah JP</td>";
 echo "<td>";
-echo "<input type='number' name='durasi_rapat' value='".($config['mode']['rapat']['durasi_jam'] ?? '30')."'>";
+echo "<input type='number' name='rapat_jumlah_jam' value='".($config['rapat']['senin_kamis']['jumlah_jam'] ?? '8')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Istirahat pertama setelah JP ke (Mode Rapat)</td>";
+echo "<td>JP pertama mulai pukul</td>";
 echo "<td>";
-echo "<input type='number' name='rapat_ist1_setelah' value='".($config['mode']['rapat']['ist1_setelah'] ?? '6')."'>";
+echo "<input type='time' name='rapat_jam_mulai' value='".($config['rapat']['senin_kamis']['jam_mulai'] ?? '08:00')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi istirahat pertama Mode Rapat</td>";
+echo "<td>Durasi JP</td>";
 echo "<td>";
-echo "<input type='number' name='rapat_ist1_durasi' value='".($config['mode']['rapat']['ist1_durasi'] ?? '15')."'>";
+echo "<input type='number' name='rapat_durasi_jam' value='".($config['rapat']['senin_kamis']['durasi_jam'] ?? '30')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Istirahat kedua setelah JP ke (Mode Rapat)</td>";
+echo "<td>Istirahat pertama setelah JP ke</td>";
 echo "<td>";
-echo "<input type='number' name='rapat_ist2_setelah' value='".($config['mode']['rapat']['ist2_setelah'] ?? '0')."'>";
+echo "<input type='number' name='rapat_ist1_setelah' value='".($config['rapat']['senin_kamis']['ist1_setelah'] ?? '4')."'>";
 echo "</td>";
 echo "</tr>";
 
 echo "<tr>";
-echo "<td>Durasi istirahat kedua Mode Rapat</td>";
+echo "<td>Durasi istirahat pertama</td>";
 echo "<td>";
-echo "<input type='number' name='rapat_ist2_durasi' value='".($config['mode']['rapat']['ist2_durasi'] ?? '0')."'>";
+echo "<input type='number' name='rapat_ist1_durasi' value='".($config['rapat']['senin_kamis']['ist1_durasi'] ?? '15')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Istirahat kedua setelah JP ke</td>";
+echo "<td>";
+echo "<input type='number' name='rapat_ist2_setelah' value='".($config['rapat']['senin_kamis']['ist2_setelah'] ?? '0')."'>";
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Durasi istirahat kedua</td>";
+echo "<td>";
+echo "<input type='number' name='rapat_ist2_durasi' value='".($config['rapat']['senin_kamis']['ist2_durasi'] ?? '0')."'>";
 echo "</td>";
 echo "</tr>";
 
@@ -218,45 +357,37 @@ HASIL GENERATE JAM
 =====================================================
 */
 
-if (!empty($jam)) {
+if (($config['mode_aktif'] ?? 'normal') == 'normal') {
 
-    echo "<h3>Hasil Jam Pelajaran</h3>";
+tampilkan_preview_jam(
+    'Preview Normal Senin-Kamis',
+    jurnalmengajar_generate_jam_preview(
+        $config['normal']['senin_kamis'] ?? []
+    )
+);
 
-    echo "<table class='generaltable'>";
+    tampilkan_preview_jam(
+        'Preview Normal Jumat',
+        jurnalmengajar_generate_jam_preview(
+            $config['normal']['jumat'] ?? []
+        )
+    );
 
-    echo "<tr>";
-    echo "<th>Jam</th>";
-    echo "<th>Mulai</th>";
-    echo "<th>Selesai</th>";
-    echo "</tr>";
+} else {
 
-    foreach ($jam as $j => $w) {
+    tampilkan_preview_jam(
+        'Preview Rapat Senin-Kamis',
+        jurnalmengajar_generate_jam_preview(
+            $config['rapat']['senin_kamis'] ?? []
+        )
+    );
 
-        echo "<tr>";
-
-        echo "<td>$j</td>";
-
-        echo "<td>{$w['mulai']}</td>";
-
-        echo "<td>{$w['selesai']}</td>";
-
-        echo "</tr>";
-
-        if (!empty($w['istirahat_setelah'])) {
-
-            echo "<tr style='background:#ffeeba; font-weight:bold;'>";
-
-            echo "<td colspan='3'>";
-
-            echo "Istirahat {$w['istirahat_setelah']} menit";
-
-            echo "</td>";
-
-            echo "</tr>";
-        }
-    }
-
-    echo "</table>";
+    tampilkan_preview_jam(
+        'Preview Normal Jumat',
+        jurnalmengajar_generate_jam_preview(
+            $config['normal']['jumat'] ?? []
+        )
+    );
 }
 
 echo $OUTPUT->footer();
--- jam_pelajaran_lib.php
+++ jam_pelajaran_lib.php
@@ -32,6 +32,113 @@ function jurnalmengajar_save_config_jam($data) {
     );
 }
 
+function jurnalmengajar_generate_jam_preview(array $cfg) {
+
+    $jumlah = $cfg['jumlah_jam'] ?? 0;
+
+    $mulai = $cfg['jam_mulai'] ?? '07:30';
+
+    $durasi = $cfg['durasi_jam'] ?? 0;
+
+    if ($jumlah <= 0 || $durasi <= 0) {
+        return [];
+    }
+
+    $ist1_after = $cfg['ist1_setelah'] ?? 0;
+    $ist1_dur   = $cfg['ist1_durasi'] ?? 0;
+
+    $ist2_after = $cfg['ist2_setelah'] ?? 0;
+    $ist2_dur   = $cfg['ist2_durasi'] ?? 0;
+
+    $jam = [];
+
+    $current = strtotime(date('Y-m-d') . ' ' . $mulai);
+
+    for ($i=1;$i<=$jumlah;$i++) {
+
+        $start = $current;
+        $end = strtotime("+{$durasi} minutes",$start);
+
+        $jam[$i]=[
+            'mulai'=>date('H:i',$start),
+            'selesai'=>date('H:i',$end),
+            'istirahat_setelah'=>false
+        ];
+
+        $current=$end;
+
+        if ($i==$ist1_after) {
+            $jam[$i]['istirahat_setelah']=$ist1_dur;
+            $current=strtotime("+{$ist1_dur} minutes",$current);
+        }
+
+        if ($i==$ist2_after) {
+            $jam[$i]['istirahat_setelah']=$ist2_dur;
+            $current=strtotime("+{$ist2_dur} minutes",$current);
+        }
+
+    }
+
+    return $jam;
+}
+
+function jurnalmengajar_get_jam_ke() {
+
+    $jam = jurnalmengajar_generate_jam();
+
+    if (empty($jam)) {
+        return 0;
+    }
+
+    $sekarang = time();
+    $hariini = date('Y-m-d');
+
+    foreach ($jam as $nomor => $w) {
+
+
+
+$mulai = strtotime($hariini . ' ' . $w['mulai']);
+$selesai = strtotime($hariini . ' ' . $w['selesai']);
+
+        if ($sekarang >= $mulai && $sekarang < $selesai) {
+            return $nomor;
+        }
+    }
+
+    return 0;
+}
+
+function jurnalmengajar_is_istirahat() {
+
+    $jam = jurnalmengajar_generate_jam();
+
+    if (empty($jam)) {
+        return false;
+    }
+
+    $sekarang = time();
+    $hariini = date('Y-m-d');
+
+    foreach ($jam as $w) {
+
+        if (empty($w['istirahat_setelah'])) {
+            continue;
+        }
+
+        $selesaijp = strtotime($hariini . ' ' . $w['selesai']);
+
+        $akhiristirahat = strtotime(
+            '+' . $w['istirahat_setelah'] . ' minutes',
+            $selesaijp
+        );
+
+        if ($sekarang >= $selesaijp && $sekarang < $akhiristirahat) {
+            return true;
+        }
+    }
+
+    return false;
+}
 function jurnalmengajar_generate_jam() {
 
     $config = jurnalmengajar_get_config_jam();
@@ -40,70 +147,41 @@ function jurnalmengajar_generate_jam() {
         return [];
     }
 
-    $jumlah = $config['jumlah_jam'] ?? 0;
+    $mode = $config['mode_aktif'] ?? 'normal';
 
-    $mulai = $config['jam_mulai'] ?? '07:30';
+    // 1=Senin ... 7=Minggu
+    $hari = (int)date('N');
 
-    $modeaktif = $config['mode_aktif'] ?? '';
+    // Jumlah hari sekolah dari settings plugin.
+    $harisekolah = (int)get_config(
+        'local_jurnalmengajar',
+        'harisekolah'
+    );
 
-    $mode = $config['mode'][$modeaktif] ?? [];
+    if ($harisekolah <= 0) {
+        $harisekolah = 5;
+    }
 
-    $durasi = $mode['durasi_jam'] ?? 0;
-
-    if ($durasi <= 0) {
+    // Di luar hari sekolah.
+    if ($hari > $harisekolah) {
         return [];
     }
 
-    $ist1_after = $mode['ist1_setelah'] ?? 0;
+    if ($hari == 5) {
 
-    $ist1_dur = $mode['ist1_durasi'] ?? 0;
+        // Jumat selalu memakai konfigurasi Normal Jumat.
+        $cfg = $config['normal']['jumat'] ?? [];
 
-    $ist2_after = $mode['ist2_setelah'] ?? 0;
+    } else {
 
-    $ist2_dur = $mode['ist2_durasi'] ?? 0;
-
-    $jam = [];
-
-    $current = strtotime($mulai);
-
-    for ($i = 1; $i <= $jumlah; $i++) {
-
-        $start = $current;
-
-        $end = strtotime(
-            "+$durasi minutes",
-            $start
-        );
-
-        $jam[$i] = [
-            'mulai' => date('H:i', $start),
-            'selesai' => date('H:i', $end),
-            'istirahat_setelah' => false
-        ];
-
-        $current = $end;
-
-        if ($i == $ist1_after) {
-
-            $jam[$i]['istirahat_setelah'] = $ist1_dur;
-
-            $current = strtotime(
-                "+$ist1_dur minutes",
-                $current
-            );
-        }
-
-        if ($i == $ist2_after) {
-
-            $jam[$i]['istirahat_setelah'] = $ist2_dur;
-
-            $current = strtotime(
-                "+$ist2_dur minutes",
-                $current
-            );
+        // Senin-Kamis (dan Sabtu bila harisekolah=6).
+        if ($mode == 'rapat') {
+            $cfg = $config['rapat']['senin_kamis'] ?? [];
+        } else {
+            $cfg = $config['normal']['senin_kamis'] ?? [];
         }
     }
 
-    return $jam;
+    return jurnalmengajar_generate_jam_preview($cfg);
 }
 ?>
